fedf204f064a36bb76ebc725d16903d05b32d921
ricardoveiga1 <ricardoveiga.ti@gmail.com>
2022-02-01 23:29:19 -0300

    Hooks

--- app/src/test/java/gradle/bdd/test/ju4/steps/Config.java
+++ app/src/test/java/gradle/bdd/test/ju4/steps/Config.java
@@ -1,17 +1,18 @@
 package gradle.bdd.test.ju4.steps;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
-import io.restassured.specification.ResponseSpecification;
 
 
 public class Config {
 
     @Before  //before do cucumber, se for do junit daria problema, pois teria que extender classes
     public void Setup(){
+        System.out.println("Setup");
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();  // deixando verboso os erros
         RestAssured.baseURI = "http://localhost:12345";
         RestAssured.basePath = "/api";
@@ -28,6 +29,38 @@ public class Config {
     }
 
     private String getToken(){
+
         return "grant acess";
+    }
+//se adicionarmos mesmo order 1 na config, será executado na ordem do arquivo
+    @Before(order = 1)
+    public  void doSomething(){
+        System.out.println("hook before");
+    }
+    @Before(value = "@primeira", order = 3)
+    public  void doFirst(){
+        System.out.println("before primeiro");
+    }
+    @Before(value = "@segunda", order = 2)
+    public  void doSecond(){
+        System.out.println("before segundo");
+    }
+    @Before(value = "@terceira", order = 1)
+    public  void doThird(){
+        System.out.println("before terceiro");
+    }
+
+    //After é decremental, inicia de tras para frente
+    @After(value = "@primeira", order = 1)
+    public  void doLast(){
+        System.out.println("before primeiro");
+    }
+    @After(value = "@segunda", order = 2)
+    public  void doLast2(){
+        System.out.println("before segundo");
+    }
+    @After(value = "@terceira", order = 3)
+    public  void doLast3(){
+        System.out.println("before terceiro");
     }
 }
--- app/src/test/resources/features/user.feature
+++ app/src/test/resources/features/user.feature
@@ -7,6 +7,7 @@ Funcionalidade: Gerenciar de um usuário na Petsore com page object
   Qualquer coisa que faça sentido para negócio
   ALT + Enter para criar os steps definitions
 
+  @primeira @segunda @terceira
   Cenário: Cria user na loja refletindo o negócio
     Quando crio um user
     Então user é salvo no sistema
