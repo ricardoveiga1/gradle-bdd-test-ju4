54821d6982210e990cc1a98d8b26d719bab3690d
ricardoveiga1 <ricardoveiga.ti@gmail.com>
2022-02-21 23:49:27 -0300

    Sintaxe antiga do rest assured

--- app/src/test/java/gradle/bdd/test/ju4/support/api/PetApi.java
+++ app/src/test/java/gradle/bdd/test/ju4/support/api/PetApi.java
@@ -1,6 +1,10 @@
 package gradle.bdd.test.ju4.support.api;
 
 import gradle.bdd.test.ju4.support.domain.Pet;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import java.util.List;
 
@@ -14,10 +18,20 @@ public class PetApi {
     public List<Pet> getPetsByStatus(String status) {
         return given().
                 pathParam("status", status). //status da url
-                when().
+        when().
                 get(FIND_PETS_BY_STATUS_ENDPOINT).
-                then().
+        then().
                 extract().body().jsonPath().getList("", Pet.class); //por ser uma lista, preciso extrair o caminho do json, e está na RAIZ, por isso o path é "", se não teria q passar path correto
+    }
+
+    //Menos legível, porém tem mais flexibilidade do JAVA
+    public List<Pet> getPetByStatus2(String status){
+        RequestSpecification httpRequest = RestAssured.given();
+        httpRequest.pathParam("status", status);
+        Response response = httpRequest.get(FIND_PETS_BY_STATUS_ENDPOINT);
+        JsonPath jsonPath = response.body().jsonPath();
+        return jsonPath.getList("", Pet.class);
+        //return  response.body().jsonPath().getList("", Pet.class);
     }
 
 
